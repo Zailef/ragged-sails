d54cdc7e10ba7f1c9987e568502cc596ab22da98
Tom <zailef.dev@hotmail.com>
2025-11-12 00:52:49 +0000

    Add initial structure for enemy AI and player damage/death

---
+++ scenes/enemies/enemy_stats.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name EnemyStats
+
+@export var name: StringName
+@export var move_speed: float = 40.0
+@export var max_health: int = 100
+@export var damage: int = 10
+@export var damage_rate: float = 1.0

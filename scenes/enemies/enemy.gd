6cd91141558f265ef441855442512389cbbffc9d
Tom <zailef.dev@hotmail.com>
2025-11-16 20:55:05 +0000

    Add animation motion modes

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -3,16 +3,21 @@ class_name Enemy
 
 var player: Player = null
 
+enum MotionAnimationMode {
+	SINGLE,
+	UDLR,
+	LR,
+}
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var stats: EnemyStats
+@export var animation_mode: MotionAnimationMode = MotionAnimationMode.SINGLE
 
-var has_directional_animations: bool = false
 var is_player_in_hurt_area: bool = false
 var damage_rate_timer: float = 0.0
 
 func _ready() -> void:
-	_set_has_directional_animations()
 	player = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
@@ -31,27 +36,29 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func _set_has_directional_animations() -> void:
-	var frames = sprite.sprite_frames
-	has_directional_animations = (
-		frames.has_animation("move_up") and
-		frames.has_animation("move_down") and
-		frames.has_animation("move_left") and
-		frames.has_animation("move_right"))
+var _animation_strategies = {
+	MotionAnimationMode.SINGLE: _animation_single,
+	MotionAnimationMode.UDLR: _animation_udlr,
+	MotionAnimationMode.LR: _animation_lr
+}
 
 func _handle_animations(direction: Vector2) -> void:
-	var new_animation: String
-	
-	if has_directional_animations:
-		if abs(direction.x) > abs(direction.y):
-			new_animation = "move_right" if direction.x > 0 else "move_left"
-		else:
-			new_animation = "move_down" if direction.y > 0 else "move_up"
-	else:
-		new_animation = "move"
-	
+	var strategy = _animation_strategies.get(animation_mode, _animation_single)
+	var new_animation = strategy.call(direction)
 	if sprite.animation != new_animation:
 		sprite.animation = new_animation
+
+func _animation_single(_direction: Vector2) -> String:
+	return "move"
+
+func _animation_udlr(direction: Vector2) -> String:
+	if abs(direction.x) > abs(direction.y):
+		return "move_right" if direction.x > 0 else "move_left"
+	else:
+		return "move_down" if direction.y > 0 else "move_up"
+
+func _animation_lr(direction: Vector2) -> String:
+	return "move_right" if direction.x > 0 else "move_left"
 
 func _on_hit_area_area_entered(_area: Area2D) -> void:
 	is_player_in_hurt_area = true

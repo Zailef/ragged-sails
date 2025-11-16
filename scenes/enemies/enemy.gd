d35c5c941f6a4b60e1d77a38ab2e4531d378397f
Tom <zailef.dev@hotmail.com>
2025-11-16 21:14:04 +0000

    Handle mermaid attack animations

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -9,10 +9,19 @@ enum MotionAnimationMode {
 	LR,
 }
 
+enum AttackAnimationMode {
+	NONE,
+	LR,
+}
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var stats: EnemyStats
 @export var animation_mode: MotionAnimationMode = MotionAnimationMode.SINGLE
+@export var attack_strategy: AttackAnimationMode = AttackAnimationMode.NONE
+
+var is_target_left: bool = false:
+	get: return player != null and player.global_position.x < global_position.x
 
 var is_player_in_hurt_area: bool = false
 var damage_rate_timer: float = 0.0
@@ -33,7 +42,6 @@ func _physics_process(delta: float) -> void:
 
 	_handle_animations(direction)
 	_handle_damage(delta)
-
 	move_and_slide()
 
 var _animation_strategies = {
@@ -42,10 +50,22 @@ var _animation_strategies = {
 	MotionAnimationMode.LR: _animation_lr
 }
 
+var _attack_strategies = {
+	AttackAnimationMode.NONE: _attack_none,
+	AttackAnimationMode.LR: _attack_lr
+}
+
 func _handle_animations(direction: Vector2) -> void:
-	var strategy = _animation_strategies.get(animation_mode, _animation_single)
-	var new_animation = strategy.call(direction)
-	if sprite.animation != new_animation:
+	var new_animation: String = ""
+
+	if is_player_in_hurt_area:
+		var strategy = _attack_strategies.get(attack_strategy, _attack_none)
+		new_animation = strategy.call()
+	else:
+		var strategy = _animation_strategies.get(animation_mode, _animation_single)
+		new_animation = strategy.call(direction)
+
+	if sprite.animation != new_animation and new_animation != "":
 		sprite.animation = new_animation
 
 func _animation_single(_direction: Vector2) -> String:
@@ -66,6 +86,12 @@ func _on_hit_area_area_entered(_area: Area2D) -> void:
 func _on_hit_area_area_exited(_area: Area2D) -> void:
 	is_player_in_hurt_area = false
 	damage_rate_timer = 0.0
+
+func _attack_none() -> String:
+	return ""
+
+func _attack_lr() -> String:
+	return "attack_left" if is_target_left else "attack_right"
 
 func _handle_damage(delta: float) -> void:
 	if not is_player_in_hurt_area:

6923c4499ab239acaaf7fac21289cf2122ca039d
Tom <zailef.dev@hotmail.com>
2025-11-30 23:21:46 +0000

    Add weapon bar UI

---
+++ scenes/ui/weapon_slot.gd
@@ -0,0 +1,28 @@
+extends Control
+class_name WeaponSlot
+
+## A single weapon slot showing the weapon icon and level.
+
+@onready var icon_texture: TextureRect = $IconTexture
+@onready var level_label: Label = $LevelLabel
+
+var weapon_data: Resource = null
+var level: int = 1
+
+func setup(data: Resource) -> void:
+	weapon_data = data
+	level = 1
+	_update_display()
+
+func set_level(new_level: int) -> void:
+	level = new_level
+	_update_display()
+
+func _update_display() -> void:
+	if not is_inside_tree():
+		await ready
+	
+	if weapon_data and weapon_data.icon:
+		icon_texture.texture = weapon_data.icon
+	
+	level_label.text = str(level)

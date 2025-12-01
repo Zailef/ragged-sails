c4bb8668c2d201d2a12077931020b4e41d920936
Tom <zailef.dev@hotmail.com>
2025-12-01 08:49:36 +0000

    Address clanker feedback

--- scenes/player/pickup_attractor.gd
+++ scenes/player/pickup_attractor.gd
@@ -23,14 +23,14 @@ func _update_collision_shape() -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-	# Check if the area's owner is a Pickup
+	# Check if the area's parent is a Pickup
 	var pickup := area.get_parent() as Pickup
 	if pickup:
 		pickup.start_attraction(get_owner())
 
 
 func _on_area_exited(area: Area2D) -> void:
-	# Check if the area's owner is a Pickup
+	# Check if the area's parent is a Pickup
 	var pickup := area.get_parent() as Pickup
 	if pickup:
 		pickup.stop_attraction()
